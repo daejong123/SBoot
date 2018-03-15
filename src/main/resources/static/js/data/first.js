572549fbce9a24c535dfa08b5b3ca2e929029d52
lindazhong <lindazhong@weshare.com.cn>
2018-03-15 15:15:54 +0800

    修改bug

--- src/main/resources/static/js/data/first.js
+++ src/main/resources/static/js/data/first.js
@@ -24,7 +24,6 @@ $(function () {
         //第一个实例
         table.render({
             elem: '#demo'
-            ,width: 1200
             ,height: 500
             // ,skin: 'line' //行边框风格
             // ,even: true //开启隔行背景
@@ -35,16 +34,16 @@ $(function () {
             ,limit: 10
             ,limits: [10, 20, 30]
             ,cols: [[ //表头
-                {field: 'id', title: 'ID', width:80, sort: true, fixed: 'left'}
+                {field: 'id', title: 'ID', width:50, sort: true, fixed: 'left'}
                 ,{field: 'username', title: '用户名', width:80, event:"del"} //event事件函数
                 ,{field: 'sex', title: '性别', width:80, sort: true}
                 ,{field: 'city', title: '城市', width:120}
-                ,{field: 'sign', title: '签名', width: 177}
+                ,{field: 'sign', title: '签名', width: 130}
                 ,{field: 'experience', title: '积分', width: 80, sort: true}
                 ,{field: 'score', title: '评分', width: 80, sort: true}
                 ,{field: 'classify', title: '职业', width: 80}
                 ,{field: 'wealth', title: '财富', width: 135, sort: true}
-                ,{title: '操作',toolbar: '#barDemo', width: 200} //绑定操作
+                ,{fixed: 'right',title: '操作', align:'center', toolbar: '#barDemo', width: 200} //绑定操作
             ]]
         });
 
--- src/main/resources/static/js/data/second.js
+++ src/main/resources/static/js/data/second.js
@@ -63,8 +63,8 @@ layui.use(['laydate', 'laypage', 'layer', 'table', 'carousel', 'upload', 'elemen
     //执行一个轮播实例
     carousel.render({
         elem: '#test1'
-        ,width: '100%' //设置容器宽度
         ,height: 200
+        ,width: '100%'
         ,arrow: 'none' //不显示箭头
         ,anim: 'fade' //切换动画方式
     });
